fca1f81abee2cae12a76a9d2bbe90e2b3a5908af
Guillermo Boscá <Kenhaytplays@gmail.com>
2021-09-18 20:40:17 +0000

    nothong

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import discord
+import os
 
 client = discord.Client()
 
 @client.event 
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  if message.content.startswith('%hello'):
+    await message.channel.send('Nice')
+
+client.run(os.getenv('token'))
