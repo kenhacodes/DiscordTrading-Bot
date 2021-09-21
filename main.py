f5877488c4fc6c353e87029606e5f32feee5cc01
Guillermo Boscá <Kenhaytplays@gmail.com>
2021-09-21 14:51:48 +0000

    Alpaca API cpnnection working fine

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import discord
 import os
 import alpaca_trade_api as tradeapi
-
+import requests
+import json
 
 client = discord.Client()
 
+#Alpaca API conexion
+
+API_KEY = os.getenv('paperAlpacaKey')
+SECRET_KEY = os.getenv('paperAlpacaKeySecret')
+APCA_API_KEY_ID = API_KEY
+APCA_API_SECRET_KEY = SECRET_KEY 
+BASE_URL = 'https://paper-api.alpaca.markets'
+ACCOUNT_URL = '{}/v2/account'.format(BASE_URL)
+
+testr = requests.get(ACCOUNT_URL, headers={'APCA-API-KEY-ID':API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY})
+
+print(testr.content)
+
+#Conexion a discord 
 @client.event 
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
 
+#Comando de prueba de mensaje
 @client.event
 async def on_message(message):
   if message.author == client.user:
