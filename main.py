502e3a29abc5e491e9e7e76a32155f591d3f0f2c
Guillermo Boscá <Kenhaytplays@gmail.com>
2021-09-21 19:09:35 +0000

    Some functions to buy via the bot. Not precise but works.

--- main.py
+++ main.py
@@ -4,25 +4,51 @@ import alpaca_trade_api as tradeapi
 import requests
 import json
 
-client = discord.Client()
 
-#Alpaca API conexion
+#Conexion a API Alpaca
 
 API_KEY = os.getenv('paperAlpacaKey')
 SECRET_KEY = os.getenv('paperAlpacaKeySecret')
-APCA_API_KEY_ID = API_KEY
-APCA_API_SECRET_KEY = SECRET_KEY 
+#URL's API
 BASE_URL = 'https://paper-api.alpaca.markets'
 ACCOUNT_URL = '{}/v2/account'.format(BASE_URL)
+PORTFOLIO_URL = '{}/v2/account/portfolio/history'.format(BASE_URL)
+ORDERS_URL = '{}/v2/orders'.format(BASE_URL)
+HEADERS = {'APCA-API-KEY-ID':API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
 
-testr = requests.get(ACCOUNT_URL, headers={'APCA-API-KEY-ID':API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY})
 
-print(testr.content)
-
-#Conexion a discord 
+#Conexion a discord
+client = discord.Client()
 @client.event 
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+  print('Woof! {0.user} is on!'.format(client))
+
+#Funciones prueba API Alpaca
+def get_account():
+  r = requests.get(ACCOUNT_URL, headers=HEADERS)
+  return json.loads(r.content)
+
+status=get_account().get('status')
+print (status)
+
+def create_order(symbol, qty, side, type, time_in_force):
+  data = {
+    'symbol': symbol,
+    'qty': qty,
+    'side': side,
+    'type': type,
+    'time_in_force':time_in_force
+  }
+  r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
+  return json.loads(r.content)
+
+def get_orders():
+  r = requests.get(ORDERS_URL, headers=HEADERS)
+  return json.loads(r.content)
+
+orders = get_orders()
+print('Pendent orders: ')
+print(orders)
 
 #Comando de prueba de mensaje
 @client.event
@@ -30,8 +56,8 @@ async def on_message(message):
   if message.author == client.user:
     return
   if message.content.startswith('%hello'):
-    await message.channel.send('NiceNice')
-
-
+    response = create_order('AAPL', 100, 'buy', 'market', 'gtc')
+    print (response)
+    await message.channel.send(response)
 
 client.run(os.getenv('token'))
