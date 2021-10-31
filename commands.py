db529a36044e7df5d185f4fbba184effee682cfd
Guillermo Boscá <Kenhaytplays@gmail.com>
2021-10-31 17:34:47 +0000

    Cleaner console prints

--- commands.py
+++ commands.py
@@ -4,24 +4,28 @@ import alpaca_trade_api as tradeapi
 import requests
 import json
 
+
 #Discord API
 DISCORD_URL = 'https://discord.com/api/v8'
 USER_ID_URL = '/users/{user.id}'
 
 
-def get_User(name, idUser, ):
+def get_User(name, idUser):
     #Comprueba si el usuario esta en la base de datos
     #Devuelve los datos del usuario en forma de lista
     #Lo de abajo es un placeholder hasta que la base de datos este terminada
-    
-    dinero = 1000
+    money = 1000
     pendent_orders = []
     historial = []
-    user_data = [name, idUser, dinero, pendent_orders, historial]
-    return user_data
+    user_data = [name, idUser, money, pendent_orders, historial]
+    r = "User: {}, Id: {}, Money: {} ".format(name, idUser, money)
+    return r
 
 
 #List of commands/actions
+def commands(message):
+  r ='{} xd'.format(message)
+  return  r;  
 
 #Main yellow color app
 #dba72e
@@ -37,10 +41,11 @@ def test_embed():
 
 
 def start():
+  
     return 0
 
 
-#List of stocks
+#List of stocks 
 #Shows the user a list of all the available stocks
 #Tells him if the market is open and when it opens
 
--- main.py
+++ main.py
@@ -4,114 +4,133 @@ import alpaca_trade_api as tradeapi
 import requests
 import json
 from commands import *
-import pymongo 
+import pymongo
 import flask
-
 
 #Conexion a API Alpaca
 
 API_KEY = os.getenv('paperAlpacaKey')
 SECRET_KEY = os.getenv('paperAlpacaKeySecret')
+
 #URL's API
 BASE_URL = 'https://paper-api.alpaca.markets'
 ACCOUNT_URL = '{}/v2/account'.format(BASE_URL)
 PORTFOLIO_URL = '{}/v2/account/portfolio/history'.format(BASE_URL)
 ORDERS_URL = '{}/v2/orders'.format(BASE_URL)
-HEADERS = {'APCA-API-KEY-ID':API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
+HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
 
 #Conexion MongoDB
 
 mongodbPassword = os.getenv('mongodbPassword')
 # Defines a new client.
-client = pymongo.MongoClient("mongodb+srv://kenha:{}@cluster0.46tv4.mongodb.net/DogeTradeBot?retryWrites=true&w=majority".format(mongodbPassword))
+client = pymongo.MongoClient(
+    "mongodb+srv://kenha:{}@cluster0.46tv4.mongodb.net/DogeTradeBot?retryWrites=true&w=majority"
+    .format(mongodbPassword))
 
 # Get the database (database name by default is "test")
-db = client.db_name # OR db = client.test
+db = client.db_name  # OR db = client.test
 print(db)
 
 #Conexion a discord
 client = discord.Client()
-@client.event 
+
+@client.event
 async def on_ready():
-  print('Woof! {0.user} is on!'.format(client))
+    print('Woof! {0.user} is on!'.format(client))
 
 #Funciones prueba API Alpaca
 
-#Devuelve info cuenta
-def get_account():
-  r = requests.get(ACCOUNT_URL, headers=HEADERS)
-  return json.loads(r.content)
-
-status=get_account().get('status')
-print (status)
-
-#Hace una compra de prueba a apple
 def create_order(symbol, qty, side, type, time_in_force):
-  data = {
-    'symbol': symbol,
-    'qty': qty,
-    'side': side,
-    'type': type,
-    'time_in_force':time_in_force
-  }
-  r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
-  return json.loads(r.content)
+    data = {
+        'symbol': symbol,
+        'qty': qty,
+        'side': side,
+        'type': type,
+        'time_in_force': time_in_force
+    }
+    r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
+    return json.loads(r.content)
+
+#Devuelve el estado de la cuenta de Alpaca
+def get_account():
+    r = requests.get(ACCOUNT_URL, headers=HEADERS)
+    return json.loads(r.content)
+
+status = get_account().get('status')
+print(status)
 
 #Devuelve las ordenes
 def get_orders():
-  r = requests.get(ORDERS_URL, headers=HEADERS)
-  return json.loads(r.content)
+    r = requests.get(ORDERS_URL, headers=HEADERS)
+    return json.loads(r.content)
 
 orders = get_orders()
 
 #Imprime las ordenes pendientes
 def print_orders():
-  print('Pendent orders: ')
-  for order in orders:
-    r = orders[0].get('symbol')    
-    print(r)
-    r = orders[0].get('qty')
-    print(r)
-    r= orders[0].get('side')
-    print(r)
-    print('..........')
+    print('Pendent orders: ')
+    for order in orders:
+        r = orders[0].get('symbol') + ' ' + orders[0].get(
+            'qty') + ' ' + orders[0].get('side')
+        print(r)
+        print('..........')
+
+
 print_orders()
 
 #Admin Permissions
-admin = 'KenHa#5259'
+adminid = '377180298066001921'
 not_admin = 'You are not an admin'
+
 
 #Comando de prueba de mensaje
 @client.event
 async def on_message(message):
-  
-  if message.author == client.user:
-    return
 
-  #Variables generales para funciones
-  name = message.author
-  idUser = message.author.id
-  
-  if message.content.startswith('%hello'):
-    response = create_order('AAPL', 100, 'buy', 'market', 'gtc')
-    print (response)
-    await message.channel.send(response)
-  if message.content.startswith('%print_orders'):
-    if str(message.author) == admin:
-      #Print las ordenes pendientes
-      print_orders()
-      print('Done!')
-      #Print los datos del usuario que escribio
-      user_data = get_User(name, idUser)
-      print (user_data)
-      #Respuesta del bot
-      await message.channel.send('Done! Woof!')
-    else:
-      await message.channel.send(not_admin)
-      print('{} tried to do a admin command!'.format(message.author))
-  
-  
+    if message.author == client.user:
+        return
+    if message.author.bot == True:
+        return
+
+    #Variables generales para funciones
+    name = message.author
+    idUser = message.author.id
+
+    #Print los datos del usuario que escribio
+    print(get_User(name, idUser))
+
+    if message.content.startswith('%buy'):
+        response = create_order('AAPL', 5, 'buy', 'market', 'gtc')
+        r = "Stock: {}, Side: {}, Quantity: {}, Time in force: {}, Status: {}".format(
+            response["symbol"], response["side"], response["qty"],
+            response["time_in_force"], response["status"])
+        print(r)
+        print("...")
+        await message.channel.send(r)
+
+    if message.content.startswith('%sell'):
+        response = create_order('AAPL', 1, 'sell', 'market', 'gtc')
+        r = "Stock: {}, Side: {}, Quantity: {}, Time in force: {}, Status: {}".format(
+            response["symbol"], response["side"], response["qty"],
+            response["time_in_force"], response["status"])
+        print(r)
+        print("...")
+        await message.channel.send(r)
+
+    #if message.content.startswith('%'):
+    #    comando = commands(message.content)[1:len(message.content)]
+    #    await message.channel.send(comando)
+
+    if message.content.startswith('%print_orders'):
+        if str(message.author.id) == adminid:
+            #Print las ordenes pendientes
+            print_orders()
+            print('Done!')
+
+            #Respuesta del bot
+            await message.channel.send('Done! Woof!')
+        else:
+            await message.channel.send(not_admin)
+            print('{} tried to do an admin command!'.format(message.author))
 
 client.run(os.getenv('token'))
-
-
